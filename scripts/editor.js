f8f4360ee5472701d408ce443a603026a905f8b5
sam <sam.may.2412@gmail.com>
2019-10-23 15:59:27 +0100

    Added outdent

--- scripts/editor.js
+++ scripts/editor.js
@@ -1,7 +1,26 @@
 "use strict";
 
 function start () {
-    window.indent.addEventListener("click", indentOnStartOfLine);
+    window.indent.addEventListener ("click", indentOnStartOfLine);
+    window.outdent.addEventListener ("click", outdentOnStartOfLine);
+}
+
+/**
+ * Event Handler for outdent on current line
+ * @param {*} e 
+ */
+function outdentOnStartOfLine (e) {
+    const rowsToSelection = getRowsToSelection();
+    const restOfRows = getPostSelectionRows();
+
+    let currentRow = rowsToSelection[rowsToSelection.length-1];
+    let outdentedLine = outdentLine(currentRow);
+
+    rowsToSelection.pop();
+    restOfRows.shift();
+    rowsToSelection.push(outdentedLine);
+
+    window.editorwindow.value = rowsToSelection.concat(restOfRows).join('\n');
 }
 
 /**
@@ -15,12 +34,18 @@ function indentOnStartOfLine (e) {
     window.editorwindow.value = indentedEditor;
 }
 
+/**
+ * Gets an array of all the rows after and including the selected line
+ */
+function getPostSelectionRows () {
+    return window.editorwindow.value.substring(window.editorwindow.selectionStart).split("\n");
+}
 
 /**
  * Gets an array of all the rows of content up to, and including the selected line 
  */
 function getRowsToSelection () {
-    return window.editorwindow.value.substr(0, window.editorwindow.selectionStart).split("\n");
+    return window.editorwindow.value.substring(0, window.editorwindow.selectionStart).split("\n");
 }
 
 /**
@@ -33,10 +58,25 @@ function getCharacterPosOfSelectedLine () {
 }
 
 /**
+ * Removes a tab character from the start of a string
+ * @param {string} str String to outdent 
+ */
+function outdentLine (str) {
+    const strA = str.split("");
+    let revChar = strA.shift();
+
+    if(revChar != '\t') {
+        strA.unshift(revChar);
+    }
+
+    return strA.join("");
+}
+
+/**
  * Splices a string with the provided value
- * @param {*} str String to splice into
- * @param {*} index Index to splice into
- * @param {*} value Value to be spliced into the string
+ * @param {string} str String to splice into
+ * @param {number} index Index to splice into
+ * @param {string} value Value to be spliced into the string
  */
 function splice (str, index, value) {
     let charsArr1 = str.slice(0, index);
