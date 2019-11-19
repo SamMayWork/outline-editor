43ef0bf85530b20a1a2b4b8056a047fce302a4ba
sam <sam.may.2412@gmail.com>
2019-11-19 15:39:38 +0000

    Added the ability to have bullet points

--- scripts/text-parser.js
+++ scripts/text-parser.js
@@ -7,6 +7,10 @@ const specChars = {
         { "md" : "\n", "html" : '<br>' },
         { "md" : "*", "html" : "<strong>", "ehtml" : "</strong>" },
         { "md" : "_", "html" : "<em>", "ehtml" : "</em>" }
+    ],
+
+    "optionalChars" : [
+        { "bullet" : "-" }
     ]
 }
 
@@ -30,6 +34,7 @@ function interpretContent (str, element) {
  */
 function convertMdToHTML(str) {
     
+    str = processBulletPoints (str);
     str = escapeStrings(str);
     str = processHeadings(str);
     
@@ -112,4 +117,40 @@ function processHeadings (str) {
     }
 
     return strA.join("");
+}
+
+/**
+ * Processes bullet points for a given line
+ * @param {*} str 
+ */
+function processBulletPoints (str) {
+    let bulletIndex = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] != "\t") {
+            bulletIndex = i;
+            break;
+        }
+    }
+
+    if (bulletIndex > 0) {
+        return splice(str, bulletIndex, "-");
+    } else {
+        return str;
+    }
+}
+
+/**
+ * Splices a string with the provided value
+ * @param {string} str String to splice into
+ * @param {number} index Index to splice into
+ * @param {string} value Value to be spliced into the string
+ */
+function splice (str, index, value) {
+    let charsArr1 = str.slice(0, index);
+    let charsArr2 = str.slice(index, str.length);
+
+    let newArr1 = charsArr1.concat(value.split(","));
+    let newArr2 = newArr1.concat(charsArr2);
+
+    return newArr2;
 }
