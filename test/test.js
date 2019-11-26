1ab080dbebe06e20609b96998a7a67f7214c79b8
sam <sam.may.2412@gmail.com>
2019-11-26 15:44:17 +0000

    Ongoing changes

--- test/test.js
+++
@@ -1,7 +0,0 @@
-function start () {
-    for (let i = 0; i < 5; i++) {
-        let item = document.querySelector (".note");
-        let clonedItem = document.importNode(item.content, true);
-        document.querySelector(".templateContainer").appendChild(clonedItem);
-    }
-}
