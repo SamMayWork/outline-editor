c170f575651f83767834cd7ef9983decf476ec6d
sam <sam.may.2412@gmail.com>
2019-12-10 12:29:13 +0000

    Adding tests

---
+++ tests/test.js
@@ -0,0 +1,43 @@
+QUnit.module("text-parser");
+QUnit.test("splice test", function (assert) {
+    assert.equal(splice("Hello, world!", 0, "Hi"), "HiHello, world!");
+    assert.equal(splice("Hello, world!", 1, "Hi"), "HHiello, world!");
+    assert.equal(splice("Hello, world!", 8, "Hi"), "Hello, wHiorld!");
+    assert.equal(splice("Hello, world!", -1, "Hi"), undefined);
+    assert.equal(splice("Hello, world!", 14, "Hi"), undefined);
+});
+
+QUnit.test("bullet test", function (assert) {
+    assert.equal(processBulletPoints("\t\tThis is a string"), "\t\t-This is a string");
+    assert.equal(processBulletPoints("\tThis is a string"), "\t-This is a string");
+    assert.equal(processBulletPoints("This is a string"), "This is a string");
+});
+
+QUnit.test("heading test", function (assert) {
+    assert.equal(processHeadings("#Hello, World!"), "<h1>Hello, World!</h1>");
+    assert.equal(processHeadings("##Hello, World!"), "<h2>Hello, World!</h2>");
+    assert.equal(processHeadings("###Hello, World!"), "<h3>Hello, World!</h3>");
+    assert.equal(processHeadings("####Hello, World!"), "<h4>Hello, World!</h4>");
+    assert.equal(processHeadings("#####Hello, World!"), "<h5>Hello, World!</h5>");
+    assert.equal(processHeadings("######Hello, World!"), "<h6>Hello, World!</h6>");
+    assert.equal(processHeadings("#######Hello, World!"), "#######Hello, World!");
+});
+
+QUnit.test("escape test", function (assert) {
+    assert.equal(escapeStrings("<><>"), "");
+    assert.equal(escapeStrings("<script>alert('Hello, world')</script>"), "scriptalert('Hello, world')/script");
+});
+
+QUnit.test ("conversion test", function (assert) {
+    assert.equal(convertMdToHTML("*This is a test*"), "<strong>This is a test</strong>");
+    assert.equal(convertMdToHTML("_This is a test_"), "<em>This is a test</em>");
+    assert.equal(convertMdToHTML("\tThis is a test"), "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-This is a test");
+    assert.equal(convertMdToHTML("#*_This is a test_*"), "<h1><strong><em>This is a test</em></strong></h1>");
+    assert.equal(convertMdToHTML("1\n2"), "1<br>2");
+});
+
+QUnit.test ("overall conversion test", function (assert) {
+    let newTextArea = document.createElement("textarea");
+    interpretContent("#This is a title\n\t\tThis is also a test", newTextArea);
+    assert.equal(newTextArea.value, "<h1>This is a title</h1><br>            -This is also a test");
+});
