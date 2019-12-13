9d320808d75a2a1648b05f3b930c222a7beca94c
sam <sam.may.2412@gmail.com>
2019-12-13 02:18:17 +0000

    small changes

--- backend/commands.sql
+++ backend/commands.sql
@@ -1,6 +1,5 @@
 CREATE TABLE DisplayNotes (
     note_id varchar(8) PRIMARY KEY,
-    note_date_created  date NOT NULL,
     note_title  varchar(50),
     note_content TEXT
 );
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const model = require("./db-model");
+const url = require("url");
 const app = express();
 
-app.use(express.static("."));
+//app.use(express.static("."));
+
+app.get("/api", async (req, res) => {
+    console.log("request");
+    //const parsedURL = url.parse(req, true);
+    //const content = await getNote(parsedURL.query.id);
+    //res.json(content);
+    res.end();
+});
+
+app.post("/api/savenote", (req, res) => {
+    res.end();
+});
 
 app.get("*", function (req, res) {
     res.end("file not found");
 });
 
-app.listen(8080);
+app.listen(8080, (err) => {console.log("err ", err);});
