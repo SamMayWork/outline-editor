842a3eb966ce5141b0baa7e1b97c5c4790b48f3c
sam <sam.may.2412@gmail.com>
2019-12-27 23:45:28 +0000

    Ongoing changes

--- backend/server.js
+++ backend/server.js
@@ -1,76 +1,139 @@
-const express = require("express");
-const model = require("./db-model");
-const url = require("url");
-const bodyParser = require("body-parser");
-const app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const Postgres = require('pg').Client;
+const argv = require('yargs');
 
-const Postgres = require("pg").Client;
+const configuration = {
+  nodb: false,
+  port: 8080,
+  verbose: false,
+  logRequests: false,
+};
 
-const sql = new Postgres({
-    database: 'oeditbackend',
-    statement_timeout: 5000,
-    host: '/var/run/postgresql'
-});
-
-sql.connect();
-
-sql.on("error", (err) => {
-    console.log(err);
-    sql.end();
-});
-
-
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
-
-app.use(express.static("."));
-
-app.get("/api", async (req, res) => {
-    console.log("request");
-    const content = await getNote(req.query.id);
-    res.json(content);
-    res.end();
-});
-
-app.post("/api/savenote", (req, res) => {
-    displayNote(req.body.title, req.body.content);
-    res.end();
-});
-
-app.get("*", function (req, res) {
-    res.end("file not found");
-});
-
-app.listen(8080);
-
-
-async function getNote (id) {
-    console.log(`Request for ${id}`);
-    let query = "SELECT * FROM displaynotes WHERE note_id = $1";
-    let results = await sql.query(query, [id]);
-    return {
-        id : results.rows[0].note_id,
-        title : results.rows[0].note_title,
-        content : results.rows[0].note_content
-    };
-}
-
-async function displayNote (title, content) {
-    let query = "INSERT INTO displaynotes VALUES ($1, $2, $3);";
-    sql.query(query, [generateId(8), title, content]);
+/**
+ * Prints the message to the screen using a red background
+ * @param {String} error error to be printed to the screen
+ */
+function errorText(error) {
+  console.log('\x1b[41m%s\x1b[0m', error);
 }
 
 /**
  * Generates a psuedo-random ID of a given length
  * @param {number} length The length of the ID to return
  */
-function generateId (length) {
-    const values = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-    let generatedId = [];
+function generateId(length) {
+  const values = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  const generatedId = [];
 
-    for (let i = 0; i < length; i++) {
-        generatedId.unshift(values[Math.floor(Math.random() * values.length)]);
+  for (let i = 0; i < length; i++) {
+    generatedId.unshift(values[Math.floor(Math.random() * values.length)]);
+  }
+
+  return generatedId.join('');
+}
+
+/**
+ * Starts the DB for the application
+ */
+function startDB() {
+  try {
+    const sql = new Postgres({
+      database: 'oeditbackend',
+      statement_timeout: 5000,
+      host: '/var/run/postgresql',
+    });
+
+    console.log('Connecting to the Database');
+    sql.connect();
+
+    sql.on('error', (err) => {
+      console.log(err);
+      sql.end();
+    });
+  } catch (error) {
+    console.log(error);
+    errorText('Unable to connect to the Database Resource, continuing starting server without attached DB');
+  }
+}
+
+/**
+ * Attempts to get the title and content of a note using its ID
+ * @param {String} id The ID of the note to find in the database
+ * @param {*} sql An SQLConnection to pull the information from
+ */
+async function getNote(id, sql) {
+  console.log(`Request for ${id}`);
+  const query = 'SELECT * FROM displaynotes WHERE note_id = $1';
+  const results = await sql.query(query, [id]);
+  return {
+    id: results.rows[0].note_id,
+    title: results.rows[0].note_title,
+    content: results.rows[0].note_content,
+  };
+}
+
+/**
+ * Stores the title and content of a note into the database
+ * @param {String} title The title of the note to save
+ * @param {String} content The content of the note to save
+ * @param {*} sql An SQLConnection to pull the information from
+ */
+async function displayNote(title, content, sql) {
+  const query = 'INSERT INTO displaynotes VALUES ($1, $2, $3);';
+  sql.query(query, [generateId(8), title, content]);
+}
+
+function start() {
+  if (argv.nodb) { configuration.nodb = true; }
+  if (argv.port !== undefined) { configuration.port = argv.port; }
+  if (argv.verbose) { configuration.verbose = true; }
+
+  if (configuration.verbose) { console.log('Starting the express server...'); }
+  const app = express();
+
+  if (configuration.nodb === false) {
+    console.log('Starting the Database...');
+    startDB();
+  }
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+
+  // Serve the static content
+  app.use(express.static('.'));
+
+  // #region HTTP paths
+
+  app.get('/api', async (req, res) => {
+    console.log('request');
+    const content = await getNote(req.query.id);
+    res.json(content);
+    res.end();
+  });
+
+  app.post('/api/savenote', (req, res) => {
+    if (configuration.nodb) {
+      res.status('404');
+      res.end();
     }
 
-    return generatedId.join("");
+    displayNote(req.body.title, req.body.content);
+    res.end();
+  });
+
+  app.get('*', (req, res) => {
+    res.end('file not found');
+  });
+
+  // #endregion
+
+  if (configuration.verbose) { console.log(`Starting listening on the port ${configuration.port}`); }
+  app.listen(configuration.port);
+}
+
+if (require.name === module) {
+  start();
+} else {
+  errorText('Server not started from module!');
 }
